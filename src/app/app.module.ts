04dbf88eac00ad5bc1da2009303ad522ddcc0757
zelta96 <33959678+zelta96@users.noreply.github.com>
2021-07-05 23:00:51 -0500

    empezamos con el navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    CardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
