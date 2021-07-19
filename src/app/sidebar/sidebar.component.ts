f5b3166403fc573ff0b6446440d0fa85a3d9dfa1
zelta96 <33959678+zelta96@users.noreply.github.com>
2021-07-18 22:19:29 -0500

    main style nav
    this is the main style for navabar at the moment

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars,faHome,faClock,faUsers,faCog,faDoorOpen } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.sass']
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  faBars = faBars;
+  faHome = faHome;
+  faClock = faClock;
+  faUsers = faUsers;
+  faCog = faCog;
+  faDoorOpen = faDoorOpen;
   constructor() { }
 
   ngOnInit(): void {
