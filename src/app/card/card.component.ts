4d81597a3a61e4221ae0b856aa1903b28e9325f9
acarmona <victormedina96@outlook.com>
2021-09-01 20:54:11 -0500

    add examples card to use

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Repartidores } from './../../model/repartidores.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-
+  @Input() reparetidorData:Repartidores = new Repartidores("","");
   constructor() { }
 
   ngOnInit(): void {
